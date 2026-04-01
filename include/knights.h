ef516bf396e57b7e1929425b3ce671fb973c81e7
mrgreenapple24 <mrchill2405@gmail.com>
2026-04-01 17:31:59 +0530

    ver 0.2.11
    knight functions added

--- include/knights.h
+++ include/knights.h
@@ -5,6 +5,9 @@
 #include "bitboard.h"
 
 extern U64 arrKnightAttacks[64];
-U64 knightAttacks(int sq);
-
+extern U64 sqknightAttacks(int sq);
+extern U64 knightFill(U64 knights);
+extern U64 knightAttacks(U64 knights);
+extern U64 forkTargetSquare(U64 targets);
+int knightDistance(int a, int b);
 #endif
