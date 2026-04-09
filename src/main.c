ae186b278994ffa76e670bb5207a1c2b9c7274aa
mrgreenapple24 <mrchill2405@gmail.com>
2026-04-09 15:08:29 +0530

    Ver 0.2.26
    Add a function to set occupancy and masked bit LUTs

--- src/main.c
+++ src/main.c
@@ -5,6 +5,20 @@ int main() {
     reset_board(&board);
 
     for (int i = 0; i < 64; i++) {
-        print_bitboard(queen_masks[i]);
+        printf("%d, ", count_bits(rook_masks[i]));
     }
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 64; i++) {
+        printf("%d, ", count_bits(bishop_masks[i]));
+    }
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 64; i++) {
+        printf("%d, ", count_bits(queen_masks[i]));
+    }
+    printf("\n");
 }
