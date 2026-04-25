d85a0144be0d1bfc4e78574608489d0703507731
mrgreenapple24 <mrchill2405@gmail.com>
2026-04-25 20:09:35 +0530

    ver 0.2.31
    Add Move Struct

--- include/types.h
+++ include/types.h
@@ -4,13 +4,14 @@
 #include <stdint.h>
 
 typedef uint64_t U64;
-#define max 2048;
-#define notAFile 0xfefefefefefefefe; // ~0x0101010101010101
-#define notHFile 0x7f7f7f7f7f7f7f7f; // ~0x8080808080808080
+
+#define MAX_MOVES 256
+#define notAFile 0xfefefefefefefefeULL
+#define notHFile 0x7f7f7f7f7f7f7f7fULL
 
 enum { white, black, both };
-enum { wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk };
-#define EMPTY 0
+enum { wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk, EMPTY };
+
 enum {
   a1, b1, c1, d1, e1, f1, g1, h1,
   a2, b2, c2, d2, e2, f2, g2, h2,
@@ -21,6 +22,43 @@ enum {
   a7, b7, c7, d7, e7, f7, g7, h7,
   a8, b8, c8, d8, e8, f8, g8, h8, NO_SQ, OFFBOARD
 };
+
+/**
+ * Move Bit Format:
+ * 0-5: From (6 bits)
+ * 6-11: To (6 bits)
+ * 12-15: Captured (4 bits)
+ * 16-19: Promoted (4 bits)
+ * 20: EnPassant (1 bit)
+ * 21: PawnStart (1 bit)
+ * 22: Castle (1 bit)
+ * 23: Capture Flag (1 bit)
+ */
+
+typedef struct {
+    uint32_t move;
+    int score; // Used for move ordering
+} Move;
+
+typedef struct {
+    Move moves[MAX_MOVES];
+    int count;
+} MoveList;
+
+/* Macros to extract move info */
+#define GET_FROM(m) ((m) & 0x3F)
+#define GET_TO(m) (((m) >> 6) & 0x3F)
+#define GET_CAPTURED(m) (((m) >> 12) & 0xF)
+#define GET_PROMOTED(m) (((m) >> 16) & 0xF)
+
+/* Flags */
+#define MFLAG_EP 0x100000
+#define MFLAG_PS 0x200000
+#define MFLAG_CA 0x400000
+#define MFLAG_CAP 0x800000
+
+/* Macros to pack a move */
+#define MOVE(f, t, cap, pro, fl) ((f) | ((t) << 6) | ((cap) << 12) | ((pro) << 16) | (fl))
 
 enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 }; // castling rights
 
